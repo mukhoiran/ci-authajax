4bdd8d08997b715d8f609760427865a8f7145ba4
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-07-07 12:25:21 +0700

    add dashboard page

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['login'] = 'pages/index/login';
 $route['register'] = 'pages/index/register';
+$route['dashboard'] = 'pages/index/dashboard';
